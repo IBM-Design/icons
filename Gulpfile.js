492e5273ce8e459b53f9a0ca148ab1f6b74ada7f
Michael Barlock <mbarlock@us.ibm.com>
2015-12-01 07:54:25 -0500

    Minify exported svgs
    Remove excess comments and generally extra gorp from exported svgs

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,8 @@
 var del = require("del"),
     gulp = require("gulp"),
+    runSequence = require("run-sequence"),
     shell = require("gulp-shell"),
+    svgmin = require("gulp-svgmin"),
     path = require("path");
 
 gulp.task("clean", () => {
@@ -9,13 +11,22 @@ gulp.task("clean", () => {
     ]);
 });
 
-gulp.task("export", ["clean"], shell.task([
+gulp.task("export", shell.task([
     "osascript lib/export.scpt "
         + path.resolve(__dirname, "lib/IllustratorSaveAsSvgs.jsx") + " "
         + path.resolve(__dirname, "dist") + " "
         + path.resolve(__dirname, "src") + " "
 ]));
 
-gulp.task("default", ["clean", "export"]);
+gulp.task("svg", () => {
+    return gulp.src("dist/**/*.svg")
+        .pipe(svgmin())
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task("default", done => {
+    runSequence("clean", "export", "svg", done);
+});
+
 
 
