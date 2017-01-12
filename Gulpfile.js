d846a42f1d1a243dc089a312bf57f9f617a859f5
James Y. Rauhut <jamesrauhut@us.ibm.com>
2017-01-11 22:43:38 -0600

    Added zips to dist/

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,7 +3,11 @@ var del = require("del"),
     runSequence = require("run-sequence"),
     shell = require("gulp-shell"),
     svgmin = require("gulp-svgmin"),
-    path = require("path");
+    path = require("path"),
+    zip = require('gulp-zip'),
+    merge = require('merge-stream'),
+    foreach = require('gulp-foreach'),
+    flatten = require('gulp-flatten');
 
 gulp.task("clean", () => {
     return del([
@@ -24,9 +28,46 @@ gulp.task("svg", () => {
         .pipe(gulp.dest("dist"));
 });
 
-gulp.task("default", done => {
-    runSequence("clean", "export", "svg", done);
+// Icon Download Tasks
+gulp.task('compile-zips-all', function() {
+  return gulp.src(['./dist/**/!(*.json)', '!./dist/zips/'])
+    .pipe(zip('ibm-design-icons.zip'))
+    .pipe(gulp.dest('dist/zips/'))
 });
 
+gulp.task('compile-zips-individual', ['compile-zips-all'], function() {
+  return merge(
+    gulp.src('./dist/**/*_16.svg'),
+    gulp.src('./dist/**/*_24.svg'),
+    gulp.src('./dist/**/*_32.svg'),
+    gulp.src('./dist/**/*_64.svg'),
+    gulp.src('./dist/**/*_128.svg'),
+    gulp.src('./dist/**/glyphs/*.svg')
+  ).pipe(
+    foreach(
+      function(stream, file) {
+        var iconName = function(iconPath) {
+          var baseName = path.basename(iconPath);
+          return baseName.substring(0,baseName.lastIndexOf('_'));
+        }(file.path);
+        return gulp.src(
+          [
+            './dist/**/' + iconName + '_*',
+            '!./dist/zips/',
+            './dist/LICENSE.md',
+            './dist/README.md'
+          ], {}
+        )
+        .pipe(flatten({newPath: './ibm-design-icons-' + iconName}))
+        .pipe(zip('ibm-design-icons-' + iconName + '.zip'))
+        .pipe(gulp.dest('dist/zips/'));
+      }
+    )
+  );
+});
 
+gulp.task('zips', ['compile-zips-individual']);
 
+gulp.task("default", done => {
+    runSequence("clean", "export", "svg", "zips", done);
+});
